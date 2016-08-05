3d29ff44772e6821c863472e14e39273091afedc
Joaquín Yeray Martín de la Nuez <joaquinmnsm@gmail.com>
2016-08-05 12:22:14 +0100

    Enable CORS to localhost

--- DaKingToDo.WebApi/App_Start/WebApiConfig.cs
+++ DaKingToDo.WebApi/App_Start/WebApiConfig.cs
@@ -11,6 +11,7 @@ namespace DaKingToDo.WebApi
         public static void Register(HttpConfiguration config)
         {
             // Configuración y servicios de API web
+            config.EnableCors();
 
             // Rutas de API web
             config.MapHttpAttributeRoutes();
--- DaKingToDo.WebApi/Controllers/TasksController.cs
+++ DaKingToDo.WebApi/Controllers/TasksController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Web.Http;
+using System.Web.Http.Cors;
 using DaKingToDo.Business;
 using DaKingToDo.Services;
 using NSubstitute;
@@ -8,7 +9,7 @@ namespace DaKingToDo.WebApi.Controllers
 {
     public class TasksController : ApiController
     {
-        [Route("tasks"), HttpGet]
+        [Route("tasks"), HttpGet, EnableCors(origins: "http://localhost", headers: "*", methods: "*")]
         public List<Task> GetAllTasks()
         {
             var repository = Substitute.For<TaskRepository>();
