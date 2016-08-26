48fc468c1cb6b5785a94dfdd249ef288b71b4d9b
Joaquín Yeray Martín de la Nuez <joaquinmnsm@gmail.com>
2016-08-26 17:39:59 +0100

    Use memory repository

--- DaKingToDo.WebApi/Controllers/TasksController.cs
+++ DaKingToDo.WebApi/Controllers/TasksController.cs
@@ -3,6 +3,7 @@ using System.Web.Http;
 using System.Web.Http.Cors;
 using DaKingToDo.Business;
 using DaKingToDo.Services;
+using DaKingToDo.Infraestructure;
 using NSubstitute;
 
 namespace DaKingToDo.WebApi.Controllers
@@ -12,9 +13,10 @@ namespace DaKingToDo.WebApi.Controllers
         [Route("tasks"), HttpGet, EnableCors(origins: "http://localhost", headers: "*", methods: "*")]
         public List<Task> GetAllTasks()
         {
-            var repository = Substitute.For<TaskRepository>();
+            //var repository = Substitute.For<TaskRepository>();
+            TaskRepository repository = new MemoryRepository();
             var service = new TaskService(repository);
-            repository.Load().Returns(new ToDoList { new Task("Something"), new Task("Anotherthing") });
+            //repository.Load().Returns(new ToDoList { new Task("Something"), new Task("Anotherthing") });
 
             return service.Load();
         }
---
+++ DaKingToDo/Infraestructure/MemoryRepository.cs
@@ -0,0 +1,15 @@
+﻿using DaKingToDo.Business;
+using DaKingToDo.Services;
+
+namespace DaKingToDo.Infraestructure
+{
+    public class MemoryRepository : TaskRepository
+    {
+        private ToDoList toDoList = new ToDoList {new Task("Something"), new Task("Anotherthing"), new Task("Sleep")};
+        
+        public ToDoList Load()
+        {
+            return toDoList;
+        }
+    }
+}
